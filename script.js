86560f2951522a0dc2595c9192a3c136efe7140f
team-affixe <team.affixe-7v@icloud.com>
2025-08-16 14:30:42 +0200

    Shoot animation

--- script.js
+++ script.js
@@ -1,33 +1,72 @@
 let frame = 0;
 let state = "IDLE";
 let left = 300;
+let leftArrow = false;
+let rightArrow = false;
+let attacking = false;
 
 setInterval(moveCharacter, 75);
-setInterval(updateBackground, 1000 / 60);
+setInterval(updateGame, 1000 / 60);
 
 document.onkeydown = checkKey;
+document.onkeyup = unCheckKey;
 
 function checkKey(e) {
+  e = e || window.event;
+
   if (e.keyCode == "37") {
-    // left arrow
-    left -= 5;
+    leftArrow = true;
     setState("WALK");
   } else if (e.keyCode == "39") {
-    // right arrow
-    left += 5;
+    rightArrow = true;
     setState("WALK");
+  }
+  if (e.keyCode == "68") {
+    // 'd' Taste
+    attacking = true;
   }
 }
 
-function updateBackground() {
+function unCheckKey(e) {
+  e = e || window.event;
+  if (e.keyCode == "37") {
+    leftArrow = false;
+  } else if (e.keyCode == "39") {
+    rightArrow = false;
+  }
+}
+
+function updateGame() {
   currentBackground.style.objectPosition = `${-left}px`;
+  if (leftArrow) {
+    left -= 5;
+  }
+  if (rightArrow) {
+    left += 5;
+  }
+
+  if (attacking) {
+    setState("ATTACK");
+  } else if (leftArrow || rightArrow) {
+    setState("WALK");
+  } else {
+    setState("IDLE");
+  }
 }
 
 function moveCharacter() {
   pirat.src = `img/2/2_entity_000_${state}_00${frame}.png`;
   frame++;
+  if (leftArrow) {
+    pirat.style.transform = "scaleX(-1)";
+  }
+
+  if (rightArrow) {
+    pirat.style.transform = "scaleX(1)";
+  }
 
   if (frame == 7) {
+    attacking = false;
     frame = 0;
   }
 }
