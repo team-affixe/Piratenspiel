2bce44b85db990267bd1145106314badc2f97331
team-affixe <team.affixe-7v@icloud.com>
2025-08-16 13:30:08 +0200

    Character moves

---
+++ script.js
@@ -0,0 +1,40 @@
+let frame = 0;
+let state = "IDLE";
+let left = 300;
+
+setInterval(moveCharacter, 75);
+setInterval(updateBackground, 1000 / 60);
+
+document.onkeydown = checkKey;
+
+function checkKey(e) {
+  if (e.keyCode == "37") {
+    // left arrow
+    left -= 5;
+    setState("WALK");
+  } else if (e.keyCode == "39") {
+    // right arrow
+    left += 5;
+    setState("WALK");
+  }
+}
+
+function updateBackground() {
+  currentBackground.style.objectPosition = `${-left}px`;
+}
+
+function moveCharacter() {
+  pirat.src = `img/2/2_entity_000_${state}_00${frame}.png`;
+  frame++;
+
+  if (frame == 7) {
+    frame = 0;
+  }
+}
+
+function setState(newState) {
+  if (state !== newState) {
+    frame = 0;
+    state = newState;
+  }
+}
